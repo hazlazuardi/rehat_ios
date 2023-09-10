42c7a6397665168061264661537a6c1a9fdccf5c
hazlazuardi <hazlazuardi@gmail.com>
2023-09-11 01:45:01 +1000

    feat: set up getting HR and store it as dummy

--- App.js
+++ App.js
@@ -1,10 +1,91 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StoreProvider from './src/context/Context';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabBar from './src/components/BottomTabBar';
+import AppleHealthKit from 'react-native-health';
+import { NativeEventEmitter, NativeModules } from 'react-native';
+
 
 
 function App() {
+
+
+	AppleHealthKit.isAvailable((err, available) => {
+		if (err) {
+			console.log('error initializing Healthkit: ', err);
+			return;
+		}
+		console.log(available);
+	});
+
+	let options = {
+		unit: 'bpm', // optional; default 'bpm'
+		startDate: new Date(2023, 7, 20).toISOString(), // required
+		endDate: new Date().toISOString(), // optional; default now
+		ascending: false, // optional; default false
+		limit: 10, // optional; default no limit
+	};
+
+	function getHR() {
+		return AppleHealthKit.getHeartRateSamples(
+			options,
+			(err, results) => {
+				if (err) {
+					console.log('error HR', err);
+					return;
+				}
+				console.log(results);
+			},
+		);
+	}
+
+	getHR();
+
+
+	new NativeEventEmitter(NativeModules.AppleHealthKit).addListener(
+		'healthKit:HeartRate:new',
+		async () => {
+			console.log('--> observer triggered');
+			getHR();
+		},
+	);
+	// /* Permission options */
+	// const permissions = {
+	// 	permissions: {
+	// 		read: [AppleHealthKit.Constants.Permissions.HeartRate],
+	// 		write: [AppleHealthKit.Constants.Permissions.Steps],
+	// 	},
+	// };
+
+	// AppleHealthKit.initHealthKit(permissions, (error) => {
+	// 	/* Called after we receive a response from the system */
+
+	// 	if (error) {
+	// 		console.log('[ERROR] Cannot grant permissions!');
+	// 	}
+
+	// 	/* Can now read or write to HealthKit */
+
+	// 	const options = {
+	// 		startDate: new Date(2023, 9, 8).toISOString(),
+	// 	};
+
+	// 	AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
+	// 		/* Samples are now collected from HealthKit */
+	// 		console.log('result', results);
+	// 	});
+	// });
+
+
+	// useEffect(() => {
+	// 	new NativeAppEventEmitter(NativeModules.AppleHealthKit).addListener(
+	// 		'healthKit:HeartRate:new',
+	// 		async () => {
+	// 			console.log('--> observer triggered');
+	// 		}
+	// 	);
+	// });
+
 	return (
 		<StoreProvider>
 			<NavigationContainer>
--- ios/rehat_ios/AppDelegate.mm
+++ ios/rehat_ios/AppDelegate.mm
@@ -2,15 +2,25 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+/* Add the library import at the top of AppDelegate.m */
+#import "RCTAppleHealthKit.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Create a bridge instance
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self
+                                            launchOptions:launchOptions];
+  
   self.moduleName = @"rehat_ios";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  // Add background initializer for HealthKit
+  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
