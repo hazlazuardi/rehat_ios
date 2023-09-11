67a7e0ea90bfff46f70d8737db7f5dc71db66816
hazlazuardi <hazlazuardi@gmail.com>
2023-09-12 00:00:19 +1000

    style: extract views

--- ios/rehat Watch App/ContentView.swift
+++ ios/rehat Watch App/ContentView.swift
@@ -18,37 +18,6 @@ struct ContentView: View {
   }
 }
 
-struct EmergencyContactView: View {
-  var body: some View {
-    VStack {
-      Button("Call Emergency Contact") {}
-    }
-  }
-}
-
-struct TherapyView: View {
-  @State private var showToolbarTitle = false
-  
-  
-  var body: some View {
-    VStack {
-      List {
-        ForEach(0..<10, id: \.self) { index in
-          NavigationLink(destination: Text("Technique View \(index)")) {
-            Text("Technique \(index)")
-          }
-        }
-      }
-    }
-    .navigationTitle("Therapy")
-    .toolbar {
-      ToolbarItem(placement: .primaryAction) {
-        Button("Add") {
-        }
-      }
-    }
-  }
-}
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
