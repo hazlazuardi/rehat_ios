b69de43e3a274cf1bb4c3b06a814e1cde9783c3f
hazlazuardi <hazlazuardi@gmail.com>
2023-09-28 01:48:59 +1000

    feat: navigation using NavigationSplitView

---
+++ ios/rehat Watch App/PanicView.swift
@@ -0,0 +1,18 @@
+//
+//  PanicView.swift
+//  rehat Watch App
+//
+//  Created by Wishnu Hazmi Lazuardi on 28/9/2023.
+//
+
+import SwiftUI
+
+struct PanicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PanicView()
+}
