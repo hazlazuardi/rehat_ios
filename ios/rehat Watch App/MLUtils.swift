03c3ce05b8d3e8a3cd639751772b8bc7af7b86d3
Avanox <avanox@Jeromes-MacBook-Pro.local>
2023-10-03 16:01:17 +1000

    feat: add additional utils for parsing csv and retraining model

--- ios/rehat Watch App/MLUtils.swift
+++ ios/rehat Watch App/MLUtils.swift
@@ -30,29 +30,70 @@ func predict(hr: Double, sdnn: Double) -> PanicAttackClassifierOutput {
 
 // Given existing data and current measurements & state,
 // updates the model
-func retrain(existing: MLDictionaryFeatureProvider, hr: Double, sdnn: Double, label: Int64) throws {
-  let hrFeature = MLFeatureValue(double: hr)
-  let sdnnFeature = MLFeatureValue(double: sdnn)
-  let labelFeature = MLFeatureValue(int64: label)
+func retrain(sdnnData: [Double], hrData: [Double], labelData: [Int64], hr: Double, sdnn: Double, label: Int64) throws {
+  let hrFeatures = doubleArrayToFeatureArray(doubleArray: hrData) + [MLFeatureValue(double: hr)]
+  let sdnnFeatures = doubleArrayToFeatureArray(doubleArray: sdnnData) + [MLFeatureValue(double: sdnn)]
+  let labelFeatures = intArrayToFeatureArray(intArray: labelData) + [MLFeatureValue(int64: label)]
   
   // create new feature provider from new data
-  var featureProviders = [MLFeatureProvider]()
-  featureProviders.append(existing)
-  let dataPointFeatures: [String:MLFeatureValue] = ["hr": hrFeature,
-                                                    "sdnn": sdnnFeature,
-                                                    "label": labelFeature]
-  if let provider = try? MLDictionaryFeatureProvider(dictionary: dataPointFeatures) {
-    featureProviders.append(provider)
-  } else {
-    print("Error creating ML retraining data")
-  }
+//  var featureProviders = [MLFeatureProvider]()
+//  featureProviders.append(existing)
+//  let dataPointFeatures: [String:MLFeatureValue] = ["hr": hrFeature,
+//                                                    "sdnn": sdnnFeature,
+//                                                    "label": labelFeature]
+  let featureDict = [
+    "sdnn": sdnnFeatures,
+    "hr": hrFeatures,
+    "label": labelFeatures
+  ]
+//  if let provider = try? MLDictionaryFeatureProvider(dictionary: featureDict) {
+//    featureProviders.append(provider)
+//  } else {
+//    print("Error creating ML retraining data")
+//  }
   
   // FIXME: this code kinda stinks
-  if let url = URL(string: "PanicAttackClassifier.mlmodelc") {
-    let arrayBatchProvider = MLArrayBatchProvider(array: featureProviders)
+  if let url = Bundle.main.url(forResource: "PanicAttackClassifier", withExtension: "mlmodelc") {
+    let arrayBatchProvider = try MLArrayBatchProvider(dictionary: featureDict)
     // FIXME: Debug print
-    guard let updateTask = try? MLUpdateTask(forModelAt: url, trainingData: arrayBatchProvider, configuration: nil, completionHandler: {(MLUpdateContext) -> () in print("Successfully updated model.")})
-    else { print("Error updating model!"); return }
+    // Update Task
+    do {
+      let updateTask = try MLUpdateTask(forModelAt: url,
+                                               trainingData: arrayBatchProvider,
+                                               configuration: nil,
+                                               completionHandler: completionHandler
+                                            )
+      updateTask.resume()
+    } catch {
+      print("\(error)")
+    }
+//    else { print("Error creating update task!"); return }
+  } else {
+    print("ML Model not found!")
   }
-  print("ML Model not found!")
 }
+
+func completionHandler(updateContext: MLUpdateContext) -> Void {
+  let updatedModel = updateContext.model
+  let fileManager = FileManager.default
+  let url = Bundle.main.url(forResource: "PanicAttackClassifier", withExtension: "mlmodelc")?.absoluteURL
+  
+  do {
+    try updatedModel.write(to: url!)
+  } catch {
+    print("Error writing updated model to file!")
+  }
+  print("Successfully updated model!")
+}
+
+func doubleArrayToFeatureArray(doubleArray: [Double]) -> [MLFeatureValue] {
+  return doubleArray.map { MLFeatureValue(double: $0) }
+}
+
+func intArrayToFeatureArray(intArray: [Int64]) -> [MLFeatureValue] {
+  return intArray.map { MLFeatureValue(int64: $0) }
+}
+
+//func getDataset() -> MLDictionaryFeatureProvider {
+//  let datasetURL = Bundle.main.url(forResource: "all_subjects_dataset", withExtension: ".csv")
+//}
