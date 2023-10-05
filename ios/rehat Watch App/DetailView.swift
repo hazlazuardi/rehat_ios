3caf60c8ff45d514a26d26e610034914c5343db6
KevinAfiza <afizakevin@gmail.com>
2023-10-05 17:44:34 +1000

    feat: switch cognitive reconstruction to vertical so it doesn't collide with horizontal tabbing

--- ios/rehat Watch App/DetailView.swift
+++ ios/rehat Watch App/DetailView.swift
@@ -8,42 +8,44 @@
 import SwiftUI
 
 struct DetailView: View {
-    var therapy: Therapy
-
+  var therapy: Therapy
+  
   var body: some View {
     VStack {
       TabView {
         ForEach(therapy.slides, id: \.self.title) { slide in
-          VStack {
-            Text(slide.title)
-              .font(.title3)
-              .foregroundStyle(.blue)
-              .padding()
-            
-            Spacer()
-            
-            ScrollView{
+          ScrollView{
+            VStack {
+              Text(slide.title)
+                .font(.title3)
+                .foregroundStyle(.blue)
+                .padding()
+              
+              Spacer()
+              
+              
               Text(slide.subTitle)
                 .font(.caption2)
                 .padding()
-
+              
             }
           }
         }
       }
       .containerBackground(.green.gradient, for: .navigation)
-//      .tint(.teal)
-//      .containerBackground(.teal.gradient, for: .tabView)
+      //      .tint(.teal)
+      //      .containerBackground(.teal.gradient, for: .tabView)
       .navigationTitle(therapy.name)
-      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+      //      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+      .tabViewStyle(.verticalPage)
     }
-//    .background(
-//      LinearGradient(
-//                    gradient: Gradient(colors: [.blue, .green]),
-//                    startPoint: .top,
-//                    endPoint: .bottom
-//      )
-//      .ignoresSafeArea(edges: .all)
-//    )
+    //    .background(
+    //      LinearGradient(
+    //                    gradient: Gradient(colors: [.blue, .green]),
+    //                    startPoint: .top,
+    //                    endPoint: .bottom
+    //      )
+    //      .ignoresSafeArea(edges: .all)
+    //    )
   }
 }
