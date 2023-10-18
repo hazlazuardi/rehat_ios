e908a5a6ea04a83761da85e9df8e37467c18f25b
Syafa Kamila <syafakamila@Syafas-MacBook-Air.local>
2023-10-18 21:03:37 +1000

    fix: fix folder structuring for all scerens, fix skip for now button in onboarding screen, added empty state for emergency contacts, journaling, and thoughts reframing, fix dashboard merge issue

--- src/screens/journaling/Journaling.js
+++ src/screens/journaling/Journaling.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { Image, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native';
-import { useJournal } from '../../context/Context';
-import { storage } from '../../../App';
-import { colors, sizes, styles } from '../../data/theme';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+import {useJournal} from '../../context/Context';
+import {storage} from '../../../App';
+import {colors, sizes, styles} from '../../data/theme';
 import PrimaryButton from '../../components/PrimaryButton';
-import { trigger } from 'react-native-haptic-feedback';
+import {trigger} from 'react-native-haptic-feedback';
 import BlurredEllipsesBackground from '../../components/BlurredEllipsesBackground';
 import assets from '../../data/assets';
 import useFormattedDate from '../../helpers/useDateFormatter';
-import { formatDate, toAssetCase } from '../../helpers/helpers';
+import {formatDate, toAssetCase} from '../../helpers/helpers';
 import EmotionCategoryButton from '../../components/journaling/EmotionCategoryButton';
 import JournalingCTACard from '../../components/journaling/JournalingCTACard';
 import useManageJournaling from '../../helpers/useManageJournaling';
-
+import EmptyState from '../../components/EmptyState';
 
 // TODO: Sort Journaling List based on dateAdded
 
@@ -24,81 +31,99 @@ import useManageJournaling from '../../helpers/useManageJournaling';
  * @param {JournalingProps} props - The component's properties.
  * @returns {JSX.Element} The rendered Journaling component.
  */
-function Journaling({ navigation }) {
+function Journaling({navigation}) {
+  const {journals, getAllJournals} = useManageJournaling();
 
-    const {
-        journals,
-        getAllJournals
-    } = useManageJournaling()
+  useEffect(() => {
+    getAllJournals();
+  }, []);
 
-    useEffect(() => {
-        getAllJournals()
-    }, []);
+  // console.log('journals', [...journals])
 
-    // console.log('journals', [...journals])
+  return (
+    <BlurredEllipsesBackground>
+      <ScrollView style={{flex: 1}} contentInsetAdjustmentBehavior="automatic">
+        <SafeAreaView>
+          <View style={{gap: sizes.gap.lg, padding: sizes.padding.md}}>
+            <View style={{gap: sizes.gap.sm}}>
+              <Text style={{...styles.text.header1}}>Journaling</Text>
 
-    return (
-        <BlurredEllipsesBackground>
-            <ScrollView style={{ flex: 1 }} contentInsetAdjustmentBehavior='automatic'>
-                <SafeAreaView>
-                    <View style={{ gap: sizes.gap.lg, padding: sizes.padding.md }}>
+              {/* Card */}
+              <JournalingCTACard
+                title={'Journaling'}
+                description={'Another day, another story. Share yours now!'}
+                buttonText={'Check in'}
+                icon={assets.icons.journaling}
+                onPress={() => navigation.navigate('Journal Category')}
+              />
+            </View>
 
-                        <View style={{ gap: sizes.gap.sm }}>
-                            <Text style={{ ...styles.text.header1 }}>Journaling</Text>
+            {/* My Journals */}
+            <View style={{gap: sizes.gap.md}}>
+              {/* Section Title */}
+              <Text style={{...styles.text.header2}}>My Journals</Text>
 
-                            {/* Card */}
-                            <JournalingCTACard
-                                title={'Journaling'}
-                                description={'Another day, another story. Share yours now!'}
-                                buttonText={'Check in'}
-                                icon={assets.icons.journaling}
-                                onPress={() => navigation.navigate('Journal Category')}
-                            />
+              {/* List of My Journal */}
+              {journals.map(journal => {
+                const formattedDate = formatDate(journal.dateAdded);
+                return (
+                  <Pressable
+                    key={journal.dateAdded + journal.emotions[0]}
+                    onPress={() =>
+                      navigation.navigate('Journal Detail', {journal})
+                    }>
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'flex-start',
+                      }}>
+                      <View style={{flex: 1}}>
+                        <View
+                          style={{
+                            flex: 1,
+                            flexShrink: 1,
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            alignItems: 'flex-start',
+                            // backgroundColor: 'red'
+                          }}>
+                          <Text
+                            style={{
+                              ...styles.text.header3,
+                              paddingRight: sizes.padding.md,
+                            }}>
+                            {journal.title || 'Untitled'}
+                          </Text>
+                          <Text style={{...styles.text.captionTransparent}}>
+                            {formattedDate.dateString} at{' '}
+                            {formattedDate.timeString}
+                          </Text>
                         </View>
+                      </View>
+                      <EmotionCategoryButton
+                        variant={toAssetCase(journal.emotionCategory)}
+                        disabled
+                        width={80}
+                      />
+                    </View>
+                  </Pressable>
+                );
+              })}
 
-                        {/* My Journals */}
-                        <View style={{ gap: sizes.gap.md }} >
+              {journals.length === 0 && (
+                <EmptyState
+                  createOrAdd={'created'}
+                  subject={'Journals'}
+                  onPressTo={'Journal Category'}
+                  navigation={navigation}
+                />
+              )}
+            </View>
 
-                            {/* Section Title */}
-                            <Text style={{ ...styles.text.header2 }}>My Journals</Text>
-
-                            {/* List of My Journal */}
-                            {journals.map((journal) => {
-                                const formattedDate = formatDate(journal.dateAdded);
-                                return (
-                                    <Pressable
-                                        key={journal.dateAdded + journal.emotions[0]}
-                                        onPress={() => navigation.navigate('Journal Detail', { journal })}
-                                    >
-                                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-                                            <View style={{ flex: 1 }}>
-                                                <View style={{
-                                                    flex: 1,
-                                                    flexShrink: 1,
-                                                    flexDirection: 'column',
-                                                    justifyContent: 'space-between',
-                                                    alignItems: 'flex-start',
-                                                    // backgroundColor: 'red'
-                                                }}>
-                                                    <Text style={{ ...styles.text.header3, paddingRight: sizes.padding.md }} >
-                                                        {journal.title || 'Untitled'}
-                                                    </Text>
-                                                    <Text style={{ ...styles.text.captionTransparent }}>
-                                                        {formattedDate.dateString} at {formattedDate.timeString}
-                                                    </Text>
-                                                </View>
-                                            </View>
-                                            <EmotionCategoryButton variant={toAssetCase(journal.emotionCategory)} disabled width={80} />
-                                        </View>
-                                    </Pressable>
-                                );
-                            })}
-                        </View>
-
-
-
-                        {/* For Debugging Only */}
-                        {/* <PrimaryButton
+            {/* For Debugging Only */}
+            {/* <PrimaryButton
                             color='red'
                             text='Clear all journal'
                             onPress={() => {
@@ -106,14 +131,11 @@ function Journaling({ navigation }) {
                                 trigger('impactHeavy')
                             }}
                         /> */}
-
-
-
-                    </View>
-                </SafeAreaView>
-            </ScrollView>
-        </BlurredEllipsesBackground >
-    );
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+    </BlurredEllipsesBackground>
+  );
 }
 
 /**
@@ -129,7 +151,7 @@ function Journaling({ navigation }) {
  * @type {JournalingProps}
  */
 Journaling.propTypes = {
-    navigation: PropTypes.object.isRequired,
+  navigation: PropTypes.object.isRequired,
 };
 
 export default Journaling;
